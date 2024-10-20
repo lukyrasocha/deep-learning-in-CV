b3dc523619b3fd9b388a98b42fd15bb3c13b6448
lukyrasocha <rasocha.lukas@gmail.com>
2024-10-20 21:14:44 +0200

    main updated with training of simple enc-dec CNN and wandb, and some plotting

--- poster-2-segmentation/main.py
+++ poster-2-segmentation/main.py
@@ -1,21 +1,34 @@
 import torch
+import numpy as np
 
 from torchvision import transforms
 
 from utils.load_data import load_data
 from utils.logger import logger
+from utils.visualize import display_random_images_and_masks, visualize_predictions
+from models.train import train_model
+from models.models import EncDec
+from models.losses import bce_loss
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
+logger.info(f"Running on {DEVICE}")
 
 # Load data
 logger.working_on("Loading data for PH2")
 
-transform = transforms.Compose([
+transform_ph2 = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.ToTensor(),
 ])
 
-ph2_train_dataset = load_data('ph2', split='train', transform=transform)
-ph2_val_dataset = load_data('ph2', split='val', transform=transform)
-ph2_test_dataset = load_data('ph2', split='test', transform=transform)
+transform_drive = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+])
+
+ph2_train_dataset = load_data('ph2', split='train', transform=transform_ph2)
+ph2_val_dataset = load_data('ph2', split='val', transform=transform_ph2)
+ph2_test_dataset = load_data('ph2', split='test', transform=transform_ph2)
 
 print('PH2 Train Dataset size:', len(ph2_train_dataset))
 print('PH2 Validation Dataset size:', len(ph2_val_dataset))
@@ -24,6 +37,7 @@ print('PH2 Test Dataset size:', len(ph2_test_dataset))
 image, mask = ph2_train_dataset[1]
 print('Image shape:', image.shape)
 print('Mask shape:', mask.shape)
+assert len(np.unique(mask.numpy()[0])) == 2, "mask needs to have binary values (0,1)"
 
 # Data loaders
 ph2_train_loader = torch.utils.data.DataLoader(ph2_train_dataset, batch_size=16, shuffle=True)
@@ -32,26 +46,79 @@ ph2_test_loader = torch.utils.data.DataLoader(ph2_test_dataset, batch_size=16, s
 
 logger.working_on("Loading data for DRIVE")
 
-drive_train_dataset = load_data('drive', split='train', transform=transform)
-drive_val_dataset = load_data('drive', split='val', transform=transform)
-drive_test_dataset = load_data('drive', split='test', transform=transform)
+drive_train_dataset = load_data('drive', split='train', transform=transform_drive)
+drive_val_dataset = load_data('drive', split='val', transform=transform_drive)
+drive_test_dataset = load_data('drive', split='test', transform=transform_drive)
 
 print('DRIVE Train Dataset size:', len(drive_train_dataset))
 print('DRIVE Validation Dataset size:', len(drive_val_dataset))
 print('DRIVE Test Dataset size:', len(drive_test_dataset))
 
 # Data loaders
-drive_train_loader = torch.utils.data.DataLoader(drive_train_dataset, batch_size=4, shuffle=True)
-drive_val_loader = torch.utils.data.DataLoader(drive_val_dataset, batch_size=4, shuffle=False)
-drive_test_loader = torch.utils.data.DataLoader(drive_test_dataset, batch_size=4, shuffle=False)
+drive_train_loader = torch.utils.data.DataLoader(drive_train_dataset, batch_size=3, shuffle=True)
+drive_val_loader = torch.utils.data.DataLoader(drive_val_dataset, batch_size=3, shuffle=False)
+drive_test_loader = torch.utils.data.DataLoader(drive_test_dataset, batch_size=3, shuffle=False)
 
 image, mask = drive_train_dataset[1]
 print('Image shape:', image.shape)
 print('Mask shape:', mask.shape)
+assert len(np.unique(mask.numpy()[0])) == 2, "mask needs to have binary values (0,1)"
 
 logger.success("Data loaded")
 
-# Simple Encoder-Decoder
+# Display some images
+display_random_images_and_masks(ph2_train_dataset, figname="ph2_random.png", num_images=3)
+display_random_images_and_masks(drive_train_dataset, figname="drive_random.png", num_images=3)
+logger.success("Saved example images and masks to 'figures'")
+
+# Simple Encoder-Decoder on PH2
+
+LEARNING_RATE = 0.001
+MAX_EPOCHS = 20
+loss_fn = bce_loss
+
+
+encdec_ph2_model = EncDec(input_channels=3, output_channels=1)
+optimizer = torch.optim.Adam(encdec_ph2_model.parameters(), lr=LEARNING_RATE)
+
+# This config is used for WANDB
+config= {
+    "learning_rate": LEARNING_RATE,
+    "architecture": "Simple-Encoder-Decoder",
+    "dataset": "PH2",
+    "epochs": MAX_EPOCHS,
+    "loss_fn": "BinaryCrossEntropy",
+    "optimizer": "Adam"
+}
+
+logger.working_on("Training simple Encoder-Decoder on PH2")
+train_model(encdec_ph2_model, ph2_train_loader, ph2_val_loader, loss_fn, optimizer,wandb_config=config, num_epochs=MAX_EPOCHS, device=DEVICE)
+visualize_predictions(encdec_ph2_model, ph2_train_loader, DEVICE, figname="ph2_predictions.png", num_images=3)
+logger.success("Saved examples of predictions for Enc-Dec of PH2 to 'figures'")
+
+# Simple Encoder-Decoder on DRIVE
+LEARNING_RATE = 0.001
+MAX_EPOCHS = 20
+loss_fn = bce_loss
+
+encdec_drive_model = EncDec(input_channels=3, output_channels=1)
+optimizer = torch.optim.Adam(encdec_drive_model.parameters(), lr=LEARNING_RATE)
+
+# This config is used for WANDB
+config= {
+    "learning_rate": LEARNING_RATE,
+    "architecture": "Simple-Encoder-Decoder",
+    "dataset": "DRIVE",
+    "epochs": MAX_EPOCHS,
+    "loss_fn": "BinaryCrossEntropy",
+    "optimizer": "Adam"
+}
+
+logger.working_on("Training simple Encoder-Decoder on DRIVE")
+train_model(encdec_drive_model, drive_train_loader, drive_val_loader, loss_fn, optimizer, wandb_config=config, num_epochs= MAX_EPOCHS, device=DEVICE)
+visualize_predictions(encdec_drive_model, drive_train_loader, DEVICE, figname="drive_predictions.png", num_images=3)
+logger.success("Saved examples of predictions for Enc-Dec of DRIVE to 'figures'")
+  
 
 # U-Net
 
