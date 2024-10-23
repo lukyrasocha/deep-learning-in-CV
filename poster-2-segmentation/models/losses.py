2aedfb87d81f0c5ddf6d20553b8ba9af23a105be
Stonej29 <75672736+Stonej29@users.noreply.github.com>
2024-10-23 14:38:22 +0200

    added focal loss

--- poster-2-segmentation/models/losses.py
+++ poster-2-segmentation/models/losses.py
@@ -1,4 +1,15 @@
+import torch
 import torch.nn.functional as F
 
+# Cross entropy
 def bce_loss(y_pred, y_real):
     return F.binary_cross_entropy_with_logits(y_pred, y_real)
+
+# Focal loss    ### check if correct
+def focal_loss(y_pred, y_real, alpha=1, gamma=2):
+    pt = torch.exp(-bce_loss(y_pred, y_real))
+    return alpha * (1 - pt) ** gamma * bce_loss(y_pred, y_real)
+
+# Cross entropy with class weight adjustments
+def bce_cwa_loss(y_pred, y_real):
+    return
